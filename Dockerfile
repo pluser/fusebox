dbbfab4995f19815c14efb1965354f453b169ccb
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2020-06-04 12:42:54 +0900

    use cache for emerge process

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,6 @@ FROM gentoo/portage:latest AS portage
 FROM gentoo/stage3-amd64:latest
 
 COPY --from=portage /var/db/repos/gentoo /var/db/repos/gentoo
-VOLUME ["/fusebox"]
-
-RUN \
-echo -e 'dev-python/pip vanilla\ndev-vcs/git -perl' >> /etc/portage/package.use/base.conf && \
-emerge --quiet sys-fs/fuse dev-python/pip dev-vcs/git && \
-pip install poetry
+VOLUME ["/fusebox", "/var/cache/binpkgs", "/var/cache/distfiles"]
 	   
 CMD ["/bin/bash"]
--- testkicker.sh
+++ testkicker.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -x
 
+echo '*** Installing system packages/modules etc...'
+echo -e 'dev-python/pip vanilla\ndev-vcs/git -perl' >> /etc/portage/package.use/base.conf
+emerge --quiet --buildpkg --usepkg sys-fs/fuse dev-python/pip dev-vcs/git
+pip install poetry
+echo '*** Installing is done.'
+
 cd /fusebox
+echo '*** Installing project specific packages.'
 poetry install
+echo '*** Installing is done.'
+echo '*** Start testing...'
 poetry run python fusebox.py --help
