e939da8f4706f694832a6480b402c147bb96d13f
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2020-06-09 11:10:55 +0900

    very simple readonly paththrough filesystem is here

--- fusebox.py
+++ fusebox.py
@@ -4,6 +4,7 @@
 fusebox.py - Entry point of FUSE-powered sandbox system
 '''
 
+from collections import defaultdict
 import os
 import sys
 import stat
@@ -19,54 +20,88 @@ logger = logging.getLogger(__name__)
 import argparse
 
 class TestFS(pyfuse3.Operations):
-    def __init__(self):
+    def __init__(self, path_source):
         super().__init__()
-        self.hello_name = b"message"
-        self.hello_inode = pyfuse3.ROOT_INODE+1
-        self.hello_data = b"hello world\n"
+        self._inode_path_map = defaultdict(set)
+        self._inode_fd_map = dict()
+        self._inode_path_map[pyfuse3.ROOT_INODE].add(path_source)
+        self._lookup_count = defaultdict(int)
 
-    async def getattr(self, inode, ctx=None):
-        entry = pyfuse3.EntryAttributes()
-        if inode == pyfuse3.ROOT_INODE:
-            entry.st_mode = (stat.S_IFDIR | 0o755)
-            entry.st_size = 0
-        elif inode == self.hello_inode:
-            entry.st_mode = (stat.S_IFREG | 0o644)
-            entry.st_size = len(self.hello_data)
+    def _inode_to_path(self, inode):
+        if inode in self._inode_path_map:
+            path_set = self._inode_path_map[inode]
         else:
-            raise pyfuse3.FUSEError(errnr.ENOENT)
+            raise FUSEError(errno.ENOENT)
 
-        stamp = int(1438467123.985654 * 1e9)
-        entry.st_atime_ns = stamp
-        entry.st_ctime_ns = stamp
-        entry.st_mtime_ns = stamp
-        entry.st_gid = os.getgid()
-        entry.st_uid = os.getuid()
-        entry.st_ino = inode
+        path = next(iter(path_set)) # FIXME: not good handling for hardlinks
+        return path
+
+    def _add_path(self, inode, path_given):
+        self._lookup_count[inode] += 1
+        self._inode_path_map[inode].add(path_given)
+        return
+            
+    async def getattr(self, inode, ctx=None):
+        if inode in self._inode_fd_map:
+            return self._getattr(fd=self._inode_fd_map[inode])
+        else:
+            return self._getattr(path=self._inode_to_path(inode))
+
+    def _getattr(self, path=None, fd=None):
+        assert fd is None or path is None
+        assert not(fd is None and path is None)
+
+        try:
+            stat = os.lstat(path) if path else os.fstat(fd)
+        except OSError as exc:
+            raise FUSEError(exc.errno)
+
+        entry = pyfuse3.EntryAttributes()
+        # copy attrs from base FS.
+        for attr in ('st_ino', 'st_mode', 'st_nlink', 'st_uid', 'st_gid',
+                     'st_rdev', 'st_size', 'st_atime_ns', 'st_mtime_ns',
+                     'st_ctime_ns'):
+            setattr(entry, attr, getattr(stat, attr))
+        entry.generation = 0
+        entry.entry_timeout = 0
+        entry.attr_timeout = 0
+        entry.st_blksize = 512
+        entry.st_blocks = ((entry.st_size+entry.st_blksize-1) // entry.st_blksize)
 
         return entry
 
-    async def lookup(self, parent_inode, name, ctx=None):
-        if parent_inode != pyfuse3.ROOT_INODE or name != self.hello_name:
-            raise pyfuse.FUSEError(errno.ENOENT)
-        return self.getattr(self.hello_inode)
+    async def lookup(self, inode_parent, name, ctx=None):
+        name_dec = os.fsdecode(name)
+        path = os.path.join(self._inode_to_path(inode_parent), name_dec)
+        attr = self._getattr(path=path)
+        if name_dec != '.' and name_dec != '..':
+            self._add_path(attr.st_ino, path)
+        return attr
 
     async def opendir(self, inode, ctx):
-        if inode != pyfuse3.ROOT_INODE:
-            raise pyfuse3.FUSEError(errno.ENOENT)
         return inode
 
-    async def readdir(self, fh, start_id, token):
-        assert fh == pyfuse3.ROOT_INODE
+    async def readdir(self, inode, offset, token):
+        path = self._inode_to_path(inode)
+        entries = list()
+        for name in os.listdir(path):
+            if name == '.' or name == '..':
+                continue
+            attr = self._getattr(path=os.path.join(path, name))
+            entries.append((attr.st_ino, name, attr))
 
-        if start_id == 0:
-            pyfuse3.readdir_reply(token, self.hello_name, await self.getattr(self.hello_inode), 1)
-
-        return
+        # FIXME: result is break if entries is changed between two calls to readdir()
+        for (ino, name, attr) in sorted(entries):
+            if ino <= offset:
+                continue
+            if not pyfuse3.readdir_reply(token, os.fsencode(name), attr, ino):
+                break
+            self._add_path(ino, os.path.join(path, name))
 
 def main():
     ### parse command line ###
     parser = argparse.ArgumentParser()
+    parser.add_argument('source', type=str)
     parser.add_argument('mountpoint', type=str)
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
@@ -86,11 +121,11 @@ def main():
     root_logger.addHandler(handler)
 
     ### start filesystem ###
-    testfs = TestFS()
+    testfs = TestFS(args.source)
     fuse_options = set(pyfuse3.default_options)
     fuse_options.add('fsname=testfs')
     fuse_options.add('debug')
-    pyfuse3.init(TestFS(), args.mountpoint, fuse_options)
+    pyfuse3.init(testfs, args.mountpoint, fuse_options)
     try:
         trio.run(pyfuse3.main)
     except:
