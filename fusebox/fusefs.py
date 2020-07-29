4528d2e8c7bd6753351502a2b27397731ca2b872
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2020-07-29 13:54:16 +0900

    ACL control file should be accessable from any user

--- fusebox/fusefs.py
+++ fusebox/fusefs.py
@@ -58,7 +58,9 @@ class Fusebox(pyfuse3.Operations):
         entry = pyfuse3.EntryAttributes()
 
         if vinfo.virtual:
-            entry.st_mode = stat.S_IFREG | stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
+            entry.st_mode = stat.S_IFREG | stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR \
+                                         | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP \
+                                         | stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH
             entry.st_nlink = 1
             entry.st_uid = 0
             entry.st_gid = 0
